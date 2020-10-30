67f8210fb3959430da340880de11918b8353adef
Karolis Samuolis <karolissamuolis@gmail.com>
2020-10-30 18:32:10 +0200

    BSPWM: fix single/dual monitor script

--- sxhkd/dual.sh
+++ sxhkd/dual.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 xrandr --output DP1 --primary --mode 1920x1080 --rotate normal --output eDP1 --mode 1920x1080 --rotate normal --below DP1;
-bspc wm -r;
 bspc monitor eDP1 -d VI VII VIII IX X
 bspc monitor DP1 -d I II III IV V
+bspc wm -r;
 notify-send "xrandr" "Switched to dual-monitor";
--- sxhkd/single.sh
+++ sxhkd/single.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 xrandr --auto;
-bspc wm -r;
 bspc monitor DP1 -r
 bspc monitor eDP1 -d I II III IV V VI VII VIII IX X
+bspc wm -r;
 notify-send "xrandr" "Switched to single-monitor";
