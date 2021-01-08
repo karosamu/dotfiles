1d116dd7df8f66d80b21d1234d13e7f77ae2a5f6
Karolis Samuolis <karolissamuolis@gmail.com>
2021-01-08 21:36:59 +0200

    Fixed dunst

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -14,7 +14,7 @@ rsync -r --copy-links --exclude '.git' ~/.mozilla/firefox/*.default-release/chro
 rsync -r --copy-links --exclude '.git' $dir/startpage/termstart/ $dir/dotfiles/startpage/termstart
 rsync -r --copy-links --exclude '.git' ~/.themes/phocus/gtk-3.0/ $dir/dotfiles/phocus/gtk-3.0/
 rsync -r --copy-links --exclude '.git' ~/.themes/phocus/scss/ $dir/dotfiles/phocus/scss/
+cp $dir/.Xresources $dir/dotfiles/.Xresources
 #cp -r $HOME/Pictures/Wallpapers $dir/dotfiles
 #cp /usr/bin/pfetch $dir/dotfiles/bin/
-
 #cp $dir/scripts/sysinfo.sh $HOME/Documents/notifetch/notifetch
--- scripts/theme.sh
+++ scripts/theme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+walcache="~/.cache/wal"
 wal -i $1;
 rm $HOME/.config/discocss/custom.css;
 ln -s -f $HOME/.cache/wal/custom.css $HOME/.config/discocss/custom.css;
@@ -8,6 +8,8 @@ ln -s -f $HOME/.cache/wal/config.fish $HOME/.config/fish/config.fish
 (cd $HOME/.themes/phocus && npm run build);
 cd $HOME;
 #wal_steam;
+tail -n +3 "$walcache/colors.Xresources" > "$walcache/colors.Xresources.tmp" && mv $walcache/colors.Xresources.tmp $walcache/colors.Xresources
+ln -s -f ~/.cache/wal/colors.Xresources ~/.config/.Xresources
 killall dunst;
 bspc wm -r;
 notify-send "pywal" "Theme applied";
